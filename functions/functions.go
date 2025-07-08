6cad60135340729fae3c455d91d76f3a62b74e41
paper091 <nitishrawat009@gmail.com>
2025-07-08 13:44:00 +0530

    functions: implemented DRY

--- functions/functions.go
+++ functions/functions.go
@@ -2,37 +2,37 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"regexp"
+	"strings"
 )
 
-func main(){
+func main() {
 	sampleStr := "hELlo$ _wORlD_"
 	camelCaseResult := transformString(sampleStr, camelCase)
 	pascalCaseResult := transformString(sampleStr, pascalCase)
 	fmt.Printf("string: %v\ncamel case: %v\npascal case: %v\n", sampleStr, camelCaseResult, pascalCaseResult)
 }
 
-func transformString(str string, fn func(str string) string) string{
+// transforms string with given function
+func transformString(str string, fn func(str []string) string) string {
+	// sanitising the string
 	regex := regexp.MustCompile(`[^a-zA-Z0-9]+`)
 	santisizedWords := strings.Fields(regex.ReplaceAllString(str, " "))
-	for i, val := range santisizedWords{
-		santisizedWords[i] = strings.ToUpper(val[:1]) + strings.ToLower(val[1:])
-	}
-	finalString := strings.Join(santisizedWords, "")
-	return fn(finalString)
+	return fn(santisizedWords)
 }
 
-func pascalCase(str string) string{
-	if len(str) <= 0{
-		return strings.ToUpper(str[:1])
+// formats the words slice as per pascal format
+func pascalCase(words []string) string {
+	for i, val := range words {
+		words[i] = strings.ToUpper(val[:1]) + strings.ToLower(val[1:])
 	}
-	return strings.ToUpper(str[:1]) + str[1:]
+	resultString := strings.Join(words, "")
+	return resultString
 }
 
-func camelCase(str string) string{
-	if(len(str) <= 0){
-		return strings.ToLower(str[:1])
-	}
-	return strings.ToLower(str[:1]) + str[1:]
+// formats the words slice as per pascal format
+func camelCase(words []string) string {
+	resultString := pascalCase(words)
+	resultString = strings.ToLower(resultString[:1]) + resultString[1:]
+	return resultString
 }
